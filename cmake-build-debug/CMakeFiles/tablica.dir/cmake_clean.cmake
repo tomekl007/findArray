c38c353cd9a00042d59cccf4e627d68deeba19fe
Tomasz Lelek <tomasz.lelek@allegro.pl>
2018-02-28 19:23:06 +0100

    1

---
+++ cmake-build-debug/CMakeFiles/tablica.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tablica.dir/main.cpp.o"
+  "tablica.pdb"
+  "tablica"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tablica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
