8bfd56de360a4db1603c2ca9afce1ef9a37a14fd
Tomasz Lelek <tomasz.lelek@allegro.pl>
2018-03-10 16:52:52 +0100

    load file

--- l_files.cpp
+++ l_files.cpp
@@ -1,7 +1,21 @@
 #include <sstream>
+#include <vector>
 #include "l_files.h"
 
 using namespace std;
+
+void split(string line, int arr[], int numberOfElements) {
+    int i = 0;
+    stringstream ssin(line);
+    while (ssin.good() && i < numberOfElements) {
+        ssin >> arr[i];
+        ++i;
+    }
+    for (i = 0; i < numberOfElements; i++) {
+        cout << arr[i] << endl;
+    }
+}
+
 
 void f_otwarcie_pliku_do_odczytu(ifstream &odczyt_pliku) {
     string nazwa = "/Users/tomasz.lelek/CLionProjects/tablica/in1.txt"; //todo hardcoded
@@ -16,10 +30,28 @@ void f_otwarcie_pliku_do_odczytu(ifstream &odczyt_pliku) {
     string line;
     ifstream myfile(nazwa);
     if (myfile.is_open()) {
-        while (myfile.good()) {
+        getline(myfile, line);
+        numberOfInputDataSets = std::stoi(line);
+        cout << "number of data sets " << numberOfInputDataSets << endl;
+
+        for (int dataSetNumber = 0; dataSetNumber < numberOfInputDataSets; dataSetNumber++) {
             getline(myfile, line);
-            cout << line << endl;
+            int numberOfElements = std::stoi(line);
+            cout << "number of elements " << numberOfElements << endl;
+            int dataSet[numberOfElements];
+
+            getline(myfile, line);
+            split(line, dataSet, numberOfElements);
+            cout << "dataSet before parse " << line << endl;
+
+            getline(myfile, line);
+            int numberOfElementsToSearch = std::stoi(line);
+            int dataToFind[numberOfElementsToSearch];
+            getline(myfile, line);
+            cout << "dataSet to find before parse " << line << endl;
+            split(line, dataToFind, numberOfElementsToSearch);
         }
+
         myfile.close();
     } else cout << "Unable to open file ";
 
@@ -52,3 +84,4 @@ int f_wyszukiwanie_interpolacyjne(long long wartosc_szukana, int Ip, int Ik, lon
     }
     return Is;
 }
+
